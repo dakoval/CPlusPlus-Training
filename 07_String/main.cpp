cc81827ad290d06a489ecdcb1c56b4c8d70a9e4a
David <kovaldavid@hotmail.com>
2021-11-09 09:43:27 -0600

    Char info

--- 07_String/main.cpp
+++ 07_String/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
+#include <cctype>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char c 'a';
+    isalpha(c); // is it a letter
+    isalpha(c); //letter or digit
+    isdigit(c); // is digit
+    islower(c); //lowercase
+    isprint(c); // printable character
+    ispunct(c); //punctuation
+    isupper(c); //uppercase
+    isspace(c); //whitespace
+    
+    
+    
+    
+    
     return 0;
 }
