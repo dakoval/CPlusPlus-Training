74ddb388c401e4d4cb447186667a1ce28e4bbcf1
David <kovaldavid@hotmail.com>
2021-11-15 16:17:58 -0600

    11/15 pointers

--- 09_References&Pointers/main.cpp
+++ 09_References&Pointers/main.cpp
@@ -50,17 +50,48 @@ int main(int argc, const char * argv[]) {
     
     
     //different ways to reference array
-    int a[]{1,2,3};
+    int a[]{1,2,3,-1};
     int *a_ptr{a};
     cout<<"a is at "<<a<<endl;
     cout<<"a_ptr is at "<<a_ptr<<endl;
-    cout<<"Reference by a[1] :"<<a[1]<<endl;
-    cout<<"Reference by a_ptr[1] :"<<a_ptr[1]<<endl;
-    cout<<"Reference by *(a_ptr+1) :"<<*(a_ptr+1)<<endl;
-    cout<<"Reference by *(a+1) :"<<*(a+1)<<endl;
+    cout<<"Reference by a[1] :"<<a[1]<<endl; //array subscript notation
+    cout<<"Reference by a_ptr[1] :"<<a_ptr[1]<<endl; // pointer subscript notation
+    cout<<"Reference by *(a_ptr+1) :"<<*(a_ptr+1)<<endl; //pointer offset notation
+    cout<<"Reference by *(a+1) :"<<*(a+1)<<endl; //array offset notation
     
     
+    //Comparing the data of pointers
+    string s1 {"Dave"}, s2{"Dave"};
+    string *p1{&s1}, *p2{&s2}, *p3{&s1};
+    cout<<boolalpha;
+    cout<<"Comparing: (p1==p2) - "<<(p1==p2)<<endl;
+    cout<<"Comparing: (p1==p3) - "<<(p1==p3)<<endl;
+    cout<<"Comparing: (*p1==*p2) - "<<(*p1==*p2)<<endl;
+    cout<<"Comparing: (*p1==*p3) - "<<(*p1==*p3)<<endl;
     
+    
+    //loop using pointer
+    cout<<"--------------------"<<endl;
+    while(*a_ptr !=-1){
+        cout<<*a_ptr++<<endl;
+    }
 
+    
+    
+    
+    
+    //Pointers with constants
+    int aa{1}, bb{2};
+    const int *ptr1 {&aa};
+    //*ptr1 = 3; ERROR
+    ptr1 = &bb; // VALID
+    int *const prt2{&aa};
+    *prt2 = 5; // VALID
+    //ptr2 = &bb ERROR
+    const int *const prt3 {&aa};
+    //*prt3 = 5 ERROR
+    //prt3 = &bb ERROR
+    
+    
     return 0;
 }
