4cd78d7689eb5f1c4c9ed63602f58c077ca050a0
David <kovaldavid@hotmail.com>
2021-11-22 12:47:02 -0600

    187/359

--- 12_Constructors/main.cpp
+++ 12_Constructors/main.cpp
@@ -19,16 +19,23 @@ public:
     bool is_dead();
     
     Player(std::string name_val="None",int health_val=0,int xp_val=0);
+    Player(const Player &p);
     
+    ~Player(); //descructor
 };
 
 Player::Player(std::string name_val,int health_val,int xp_val)
     :name{name_val},health{health_val},xp{xp_val}{
     }
 
+//Copying constructor
+Player::Player(const Player &source)
+    :name{source.name},health{source.health},xp{source.xp}{
+}
 
 int main(int argc, const char * argv[]) {
     Player p{"test"};
+    Player p2{p};
     
     std::cout << "Hello, World!\n";
     return 0;
