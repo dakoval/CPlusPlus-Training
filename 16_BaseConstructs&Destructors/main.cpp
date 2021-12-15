cf95333db00bab9e8fe1caf2fa114c0f8e56220b
David <kovaldavid@hotmail.com>
2021-12-14 20:09:49 -0600

    221

--- 16_BaseConstructs&Destructors/main.cpp
+++ 16_BaseConstructs&Destructors/main.cpp
@@ -16,27 +16,32 @@ public:
     Base() : value{0} {cout<<"Base no-args constructor"<<endl;}
     Base(int x) : value{x}{cout<<"Base (int) overloaded constructor"<<endl;}
     ~Base(){cout<<"Base destructor"<<endl;}
+    int getValue(){return value;}
 };
 
 class Derived : public Base{
-    using Base::Base; // To use the corresponding constructor from Base
 private:
     int doubled_value;
 public:
-    Derived() : doubled_value {0}{cout<<"Derived no-args constructor"<<endl;}
-    Derived(int x) : doubled_value{x*2}{cout<<"Derived (int) overloaded constructor"<<endl;}
+    Derived() : Base{},doubled_value {0}{cout<<"Derived no-args constructor"<<endl;}
+    Derived(int x) : Base{x}, doubled_value{x*2}{cout<<"Derived (int) overloaded constructor"<<endl;}
     ~Derived() {cout<<"Derived destructor"<<endl;}
+    int getDoubledValue(){return doubled_value;}
 };
 
 int main(int argc, const char * argv[]) {
     cout<<"---Base------"<<endl;
     Base b;
+    cout<<b.getValue()<<endl;
     cout<<"---Base (int)------"<<endl;
     Base bb{100};
+    cout<<bb.getValue()<<endl;
     cout<<"---Derived------"<<endl;
     Derived d;
+    cout<<d.getDoubledValue()<<":"<<d.getValue()<<endl;
     cout<<"---Derived (int)------"<<endl;
     Derived dd{100};
+    cout<<dd.getDoubledValue()<<":"<<dd.getValue()<<endl;
     cout<<"---Destructors------"<<endl;
     return 0;
 }
