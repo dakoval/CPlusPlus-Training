da7814c316c1b02058275bfd748a750870421cc9
David <kovaldavid@hotmail.com>
2021-12-17 10:17:23 -0600

    233

--- 17_Polymorphism/main.cpp
+++ 17_Polymorphism/main.cpp
@@ -23,6 +23,22 @@ public:
     }
 };
 
+
+//using virtual
+class Base2{
+public:
+    virtual void say_hello() const{
+        cout<<"Base object"<<endl;
+    }
+};
+
+class Derived2: public Base2{
+public:
+    virtual void say_hello() const{
+        cout<<"Derived object"<<endl;
+    }
+};
+
 void greetings(const Base &obj){
     cout<<"hello: ";
     obj.say_hello(); //will always bind to Base say hello
@@ -38,13 +54,19 @@ int main(int argc, const char * argv[]) {
     greetings(b);
     greetings(d);
     
+    cout<<"----Using Base object----"<<endl;
     Base *ptr = new Derived();
     ptr->say_hello(); //Base say hello
     
     unique_ptr<Base> ptr1 = make_unique<Derived>();
     ptr1->say_hello();
     
+    cout<<"----Using Derived object with Virtual----"<<endl;
+    Base2 *ptr2 = new Derived2();
+    ptr2->say_hello(); //Base say hello
     
+    unique_ptr<Base2> ptr3 = make_unique<Derived2>();
+    ptr3->say_hello();
     
     return 0;
 }
