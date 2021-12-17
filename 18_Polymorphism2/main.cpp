32a96861e08a104b86d453587b1e585a238caa14
David <kovaldavid@hotmail.com>
2021-12-17 13:30:03 -0600

    239

--- 18_Polymorphism2/main.cpp
+++ 18_Polymorphism2/main.cpp
@@ -8,36 +8,63 @@
 #include <iostream>
 using namespace std;
 
-class Account{
+
+class I_Printable{
+    friend ostream &operator<<(ostream &os, const I_Printable &obj);
+public:
+    virtual void print(ostream &os) const =0;
+};
+
+ostream &operator<<(ostream &os, const I_Printable &obj){
+    obj.print(os);
+    return os;
+};
+
+
+class Account: public I_Printable{
 public:
     virtual void withdraw(double amount){
         cout<<"Account-Withdraw"<<endl;
+    }
+    virtual void print(ostream &os)const override{
+        os<<"Account Display";
     }
     virtual ~Account(){cout<<"Account: Destructor"<<endl;}
 };
 
 class Checking:public Account{
 public:
-    virtual void withdraw(double amount){
+    virtual void withdraw(double amount) override{
         cout<<"Checking-Withdraw"<<endl;
+    }
+    virtual void print(ostream &os)const override{
+        os<<"Checking Display";
     }
     virtual ~Checking(){cout<<"Checking: Destructor"<<endl;}
 };
 
 class Savings:public Account{
 public:
-    virtual void withdraw(double amount){
+    virtual void withdraw(double amount) override{
         cout<<"Savings-Withdraw"<<endl;
+    }
+    virtual void print(ostream &os)const override{
+        os<<"Savings Display";
     }
     virtual ~Savings(){cout<<"Savings: Destructor"<<endl;}
 };
+
 class Trust:public Savings{
 public:
-    virtual void withdraw(double amount){
+    virtual void withdraw(double amount) override{
         cout<<"Trust-Withdraw"<<endl;
+    }
+    virtual void print(ostream &os)const override{
+        os<<"Trust Display";
     }
     virtual ~Trust(){cout<<"Savings: Destructor"<<endl;}
 };
+
 
 void do_withdraw(Account &account, double amount){
     account.withdraw(amount);
@@ -75,6 +102,12 @@ int main(int argc, const char * argv[]) {
     do_withdraw(a2,100);
     do_withdraw(a3,100);
     do_withdraw(a4,100);
+    cout<<"--------Displaying-----"<<endl;
+    cout<<a1<<endl;
+    cout<<a2<<endl;
+    cout<<a3<<endl;
+    cout<<a4<<endl;
+    
     cout<<"Destructor--------"<<endl;
     return 0;
 }
