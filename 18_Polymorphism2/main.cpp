f679c0e54613aca1cc6f16ac2d5013e1c1d42376
David <kovaldavid@hotmail.com>
2021-12-17 12:43:34 -0600

    236

--- 18_Polymorphism2/main.cpp
+++ 18_Polymorphism2/main.cpp
@@ -31,6 +31,17 @@ public:
     }
     virtual ~Savings(){cout<<"Savings: Destructor"<<endl;}
 };
+class Trust:public Savings{
+public:
+    virtual void withdraw(double amount){
+        cout<<"Trust-Withdraw"<<endl;
+    }
+    virtual ~Trust(){cout<<"Savings: Destructor"<<endl;}
+};
+
+void do_withdraw(Account &account, double amount){
+    account.withdraw(amount);
+}
 
 int main(int argc, const char * argv[]) {
     cout<<"====Pointers===="<<endl;
@@ -47,5 +58,23 @@ int main(int argc, const char * argv[]) {
     delete p2;
     delete p3;
     
+    cout<<"By Ref-------------------"<<endl;
+    Account a;
+    Account &ref =a;
+    ref.withdraw(100);
+    
+    Trust t;
+    Account &ref1 =t;
+    ref1.withdraw(100);
+    cout<<"-------------------"<<endl;
+    Account a1;
+    Checking a2;
+    Savings a3;
+    Trust a4;
+    do_withdraw(a1,100);
+    do_withdraw(a2,100);
+    do_withdraw(a3,100);
+    do_withdraw(a4,100);
+    cout<<"Destructor--------"<<endl;
     return 0;
 }
