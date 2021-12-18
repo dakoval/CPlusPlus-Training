7e7de7bdd9d42083f167a6e8e683dc93e5e92290
David <kovaldavid@hotmail.com>
2021-12-18 04:12:59 -0600

    290

--- 21_ExceptionHandling/main.cpp
+++ 21_ExceptionHandling/main.cpp
@@ -8,11 +8,18 @@
 #include <iostream>
 using namespace std;
 
+class ExceptionA{};
+class ExceptionB{};
+
 double calc(int a, int b){
     if(a==0)
       throw 0;
     if(b==0)
         throw "abc";
+    if(a==1)
+        throw ExceptionA();
+    if(a==1)
+        throw ExceptionB();
     return static_cast<double>(a)/b;
 };
 
@@ -24,6 +31,10 @@ int main(int argc, const char * argv[]) {
     }catch(int &ex){ //if int error.
         
     }catch(string &ex){//if string error
+        
+    }catch(ExceptionA &ex){//my own exception
+    
+    }catch(ExceptionB &ex){
         
     }catch(...){ //catch all
         
